116f9bc8a7fa2563091e1f201d25776511121b18
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-02-08 13:00:44 +0700

    build:init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <input
+    type="range"
+    min="0"
+    max="100"
+    step="1"
+    v-model="seekValue"
+    @change="onSeek"
+  />
+  <audio
+    src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+    ref="audioPlayer"
+    @timeupdate="onPlaying"
+  >
+    Your browser does not support the
+    <code>audio</code> element.
+  </audio>
+  <p>{{ currentTime }}</p>
+  <div>
+    <button @click="play">play</button>
+    <button @click="pause">pause</button>
+    <button @click="stop">stop</button>
+    <button @click="setSpeed(0.5)">0.5x</button>
+    <button @click="setSpeed(1)">1x</button>
+    <button @click="setSpeed(1.5)">1.5x</button>
+    <button @click="setSpeed(2)">2x</button>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "App",
+  data() {
+    return {
+      currentTime: 0,
+      seekValue: 0,
+    };
+  },
+  methods: {
+    play() {
+      this.$refs.audioPlayer.play();
+    },
+    pause() {
+      this.$refs.audioPlayer.pause();
+    },
+    stop() {
+      const { audioPlayer } = this.$refs;
+      audioPlayer.pause();
+      audioPlayer.currentTime = 0;
+    },
+    setSpeed(speed) {
+      this.$refs.audioPlayer.playbackRate = speed;
+    },
+    onPlaying() {
+      const { audioPlayer } = this.$refs;
+      if (!audioPlayer) {
+        return;
+      }
+      this.currentTime = audioPlayer.currentTime;
+      this.seekValue = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    },
+    onSeek() {
+      const { audioPlayer } = this.$refs;
+      const seekto = audioPlayer.duration * (this.seekValue / 100);
+      audioPlayer.currentTime = seekto;
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
